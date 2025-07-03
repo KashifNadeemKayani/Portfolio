7a8ca0383b1d05aaeb7bec0433b8256c00b24e7b
Kashif <kayanikashif970@gmail.com>
2025-07-03 22:31:53 +0500

    a

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Portfolio/',
   plugins: [react()],
   server: {
     host: true, // allows access from local network (e.g., mobile)

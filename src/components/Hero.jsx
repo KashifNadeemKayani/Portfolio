b76f37f42eb81d936777507e58b0ef024efe73f7
Kashif <kayanikashif970@gmail.com>
2026-02-10 10:45:41 +0500

    hero changed gi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { motion } from 'framer-motion';
+// import profileImage from '../assets/Pic.png';
 import profileImage from '../assets/ppic.png';
 import pic from '../assets/picc.png';
 import styles from './Hero.module.css';
